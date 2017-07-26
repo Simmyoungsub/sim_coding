e39601e092c5d8285706415907952d370b0d9d12
rop2001 <rop2001@gmail.com>
2017-07-26 17:06:50 +0900

    소스 추가

---
+++ sim_coding/src/hackerrank/InsertSorting2.java
@@ -0,0 +1,55 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class InsertSorting2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		insertSorting(arr);
+	}
+	
+	public static void insertSorting(int[] arr) {
+		
+		int j = 0;//삽입될 자리를 기억하기 위해 
+		int cnt = 0;
+		
+		for(int i=1;i<arr.length;i++) {
+			int current = arr[i];
+			
+			for(j=i-1; j>=0 && current<arr[j]; j--) {
+				arr[j+1] = arr[j];
+				cnt++;
+			}		
+			arr[j+1] = current;
+		}
+		System.out.println(cnt);
+	}
+	
+	public static void insertionSort(int[] A){
+        for(int i = 1; i < A.length; i++){
+            int value = A[i];
+            int j = i - 1;
+            while(j > 0 && A[j] > value){
+                A[j + 1] = A[j];
+                j = j - 1;
+            }
+            A[j + 1] = value;
+        }
+
+        printArray(A);
+    }
+	
+	public static void printArray(int[] arr) {
+		for(int i : arr) {
+			System.out.print(i+" ");
+		}
+		System.out.println("");
+	}
+}
---
+++ sim_coding/src/hackerrank/QuickSorting1.java
@@ -0,0 +1,80 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class QuickSorting1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] ar = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			ar[i] = sc.nextInt();
+		}
+		
+		quickSorting(ar);
+		sc.close();
+	}
+	
+	public static void quickSorting(int[] ar) {
+		
+		int pivot = ar[0];
+		int[] left = partitionLeft(ar,pivot);
+		int[] right = partitionRight(ar,pivot);
+		
+		for(int i : left) {
+			System.out.print(i+" ");
+		}
+		
+		System.out.print(pivot+" ");
+		
+		for(int i : right) {
+			System.out.print(i+" ");
+		}
+	}
+	
+	public static int[] partitionLeft(int[] ar,int pivot) {
+		int[] result = null;
+		int n = 0;
+		
+		for(int i : ar) {
+			if(i<pivot) {
+				n++;
+			}
+		}
+		
+		result = new int[n];
+		int result_idx = 0;
+		
+		for(int i : ar) {
+			if(i<pivot) {
+				result[result_idx++] = i;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static int[] partitionRight(int[] ar,int pivot) {
+		int[] result = null;
+		int n = 0;
+		
+		for(int i : ar) {
+			if(i>pivot) {
+				n++;
+			}
+		}
+		
+		result = new int[n];
+		int result_idx = 0;
+		
+		for(int i : ar) {
+			if(i>pivot) {
+				result[result_idx++] = i;
+			}
+		}
+		
+		return result;
+	}
+}
